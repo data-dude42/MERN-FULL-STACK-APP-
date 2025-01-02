a06d9dc92e06618c822be2e261753b6810947a58
data-dude42 <ss8820788@gmail.com>
2025-01-02 01:08:41 -0500

    changed some logic in deleteProduct

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -47,11 +47,10 @@ export const deleteProduct = async (req, res) => {
         return res.status(404).json({ success: false, message: "Invalid Product ID" });
     }
     try {
-        await Product.findByIdAndDelete(id);
-        res.status(404).json({ success: false, message: "invalid Product ID" });
+        const deleteProduct = await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, data: deleteProduct });
     } catch (error) {
         console.log("error in deleting products:", error.message);
         res.status(500).json({ success: false, message: "server error" });
-
     }
 };
